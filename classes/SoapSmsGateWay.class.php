aadae2cfb4847d5d80d0fa4174b68c10bcb4fbc0
VoidWalker <sohanoleksa@gmail.com>
2016-02-23 23:27:45 +0200

    original

--- classes/SoapSmsGateWay.class.php
+++ classes/SoapSmsGateWay.class.php
@@ -4,6 +4,12 @@ class SoapSmsGateWay
 {
     public function sendSms($messagesData)
     {
+        foreach ($messagesData->messageList->message->Struct as $message) {
+            file_put_contents("pars.txt", $message->text . '\n', FILE_APPEND);
+        }
+
+        //file_put_contents("pars.txt", serialize($messagesData->messageList->message->Struct));
+
         $rawPost = "Input:\r\n";
         $rawPost .= file_get_contents('php://input');
         $rawPost .= "\r\n---\r\nmessageData:\r\n";
--- client.php
+++ client.php
@@ -42,7 +42,6 @@ class Request
 }
 
 // создаем объект для отправки на сервер
-$req = new Request();
 $msg1 = new Message();
 $msg1->phone = '79871234567';
 $msg1->text = 'Тестовое сообщение 1';
@@ -61,17 +60,22 @@ $msg3->text = 'Тестовое сообщение 3';
 $msg3->date = '2014-08-22T16:01:10';
 $msg3->type = 17;
 
-$req->messageList[] = $msg1;
-$req->messageList[] = $msg2;
-$req->messageList[] = $msg3;
+$messageList = new MessageList();
+$messageList->message[] = $msg1;
+$messageList->message[] = $msg2;
 
-$req->messageList = (object)$req->messageList;
+$req = new Request();
+$req->messageList = $messageList;
+
+
+//$req->messageList = (object)$req->messageList;
 var_dump($req);
 //var_dump(unserialize('O:8:"stdClass":1:{s:11:"messageList";O:8:"stdClass":1:{s:7:"message";O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 1";s:4:"date";s:22:"2013-07-21T15:00:00.26";s:4:"type";s:2:"15";}}}'));
 //var_dump(unserialize('O:8:"stdClass":1:{s:11:"messageList";O:8:"stdClass":1:{s:7:"message";O:8:"stdClass":1:{s:6:"Struct";a:3:{i:0;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 1";s:4:"date";s:22:"2013-07-21T15:00:00.26";s:4:"type";s:2:"15";}i:1;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 2";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"16";}i:2;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 3";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"17";}}}}}'));
 //var_dump(unserialize('O:8:"stdClass":1:{s:11:"messageList";O:8:"stdClass":1:{s:7:"message";O:8:"stdClass":1:{s:5:"BOGUS";a:3:{i:0;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 1";s:4:"date";s:22:"2013-07-21T15:00:00.26";s:4:"type";s:2:"15";}i:1;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 2";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"16";}i:2;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 3";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"17";}}}}}'));
 //var_dump(unserialize('O:8:"stdClass":1:{s:11:"messageList";O:8:"stdClass":1:{s:5:"BOGUS";a:3:{i:0;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 1";s:4:"date";s:22:"2013-07-21T15:00:00.26";s:4:"type";s:2:"15";}i:1;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 2";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"16";}i:2;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 3";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"17";}}}}'));
 //var_dump(unserialize('O:8:"stdClass":1:{s:11:"messageList";O:8:"stdClass":1:{s:5:"BOGUS";O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 1";s:4:"date";s:22:"2013-07-21T15:00:00.26";s:4:"type";s:2:"15";}}}'));
+var_dump(unserialize('a:2:{i:0;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 1";s:4:"date";s:22:"2013-07-21T15:00:00.26";s:4:"type";s:2:"15";}i:1;O:8:"stdClass":4:{s:5:"phone";s:11:"79871234567";s:4:"text";s:37:"Тестовое сообщение 2";s:4:"date";s:19:"2014-08-22T16:01:10";s:4:"type";s:2:"16";}}'));
 $uri = "http://{$_SERVER['HTTP_HOST']}/smsservice.wsdl.php";
 $client = new SoapClient($uri,
     array('soap_version' => SOAP_1_2));
